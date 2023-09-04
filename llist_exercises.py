d4c16f75ecc9e55b142c97d0db64af2dc6894dcf
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-09-04 13:37:43 +0100

    add pop method: works

--- llist_exercises.py
+++ llist_exercises.py
@@ -10,6 +10,11 @@ class LinkedList:
         self.tail = self.new_node
         self.length = 1
 
+    def make_empty(self):
+        self.tail = None
+        self.head = None
+        self.length = 0
+
     def print_list(self):
         if self.length == 0:
             return None
@@ -30,31 +35,55 @@ class LinkedList:
             self.tail = self.new_node
             self.length += 1
 
-
-################ until here: do with 2 pointers ##########################
     def pop(self):
+        # edge cases: list with 1 element or empty
+        if self.length == 1:
+            temp = self.head
+            self.tail = None
+            self.head = None
+            self.length = 0
+            return temp
+        elif self.length == 0:
+            return None
+        # normal case
         temp = self.head
         pre = self.head
         while temp.next != None:
-            pass
-        pass
-######################################################
+            temp = temp.next
+            if temp.next != None:
+                pre = temp
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        return temp
+
             
 
-    def make_empty(self):
-        self.tail = None
-        self.head = None
-        self.length = 0
           
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
-my_linked_list.append(4)
 
-print(my_linked_list.pop().value) 
-print(my_linked_list.pop().value) 
-print("")
+
+
 my_linked_list.print_list()
+
+print()
+print("popped element:")
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+
+
+print()
+print("\nprint list:")
+my_linked_list.print_list()
+
+print("\nlength:", my_linked_list.length)
+
 
                                                                          
