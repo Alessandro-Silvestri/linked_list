d215de63cb75906e80612cc64e84b4c666cd8f3c
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-09-06 08:56:21 +0100

    add exercise 14: find middle node

---
+++ llist_exercise_14_find_mid_node.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+
+    # WRITE FIND_MIDDLE_NODE METHOD HERE #
+    #                                    #
+    #                                    #
+    #                                    #
+    #                                    #
+    ######################################
