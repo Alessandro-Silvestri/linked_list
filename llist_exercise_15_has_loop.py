a6177f526d76fb9fec5030f3402ad06eb2e6c1d8
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-09-08 10:59:01 +0100

    has_loop better refactored using the while loop

--- llist_exercise_15_has_loop.py
+++ llist_exercise_15_has_loop.py
@@ -26,21 +26,42 @@ class LinkedList:
         slow steps one and fast steps 2, if they meet it's a loop.
         Edge cases: empty list, only 1 node, only 2 nodes
         '''
-        # edge cases: empty list, only 1 node, only 2 nodes
-        if self.head is None:
-            return False
-        elif self.head.next is None or self.head.next.next is None:
-            return False
-        # normal case
         slow = self.head
         fast = self.head
-        while True:
+        
+        '''
+        in the following loop I used 'and', I don't have to use the 'or' otherwise
+        the second part of the line will not be read
+        '''
+        while fast is not None and fast.next is not None:
+            print(f"slow: {slow.value} - next: {fast.value}")
             slow = slow.next
             fast = fast.next.next
             if slow is fast:
                 return True
-            elif fast.next is None or fast.next.next is None:
-                return False
+        return False
+
+    
+        # def has_loop(self):
+        #     '''using 2 pointers: slow and fast
+        #     slow steps one and fast steps 2, if they meet it's a loop.
+        #     Edge cases: empty list, only 1 node, only 2 nodes
+        #     '''
+        #     # edge cases: empty list, only 1 node, only 2 nodes
+        #     if self.head is None:
+        #         return False
+        #     elif self.head.next is None or self.head.next.next is None:
+        #         return False
+        #     # normal case
+        #     slow = self.head
+        #     fast = self.head
+        #     while True:
+        #         slow = slow.next
+        #         fast = fast.next.next
+        #         if slow is fast:
+        #             return True
+        #         elif fast.next is None or fast.next.next is None:
+        #             return False
 
 
 
@@ -53,8 +74,9 @@ my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
 my_linked_list_1.tail.next = my_linked_list_1.head
-print(my_linked_list_1.has_loop() ) # Returns True
 
+
+print(my_linked_list_1.has_loop()) # Returns True
 
 
 
@@ -63,9 +85,6 @@ my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
 print(my_linked_list_2.has_loop() ) # Returns False
-
-
-a = 5
 
 
 """
