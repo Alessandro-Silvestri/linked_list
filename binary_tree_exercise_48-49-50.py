8ba562de1ec3eccf7402c61d853ebd743a08cdef
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-10-02 17:24:25 +0200

    binary tree: insert method, works tests passed

--- binary_tree_exercise_48-49-50.py
+++ binary_tree_exercise_48-49-50.py
@@ -6,3 +6,35 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+    
+    def insert(self, value):
+        new_node = Node(value)
+        # if empty tree
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        # normal case
+        while temp:
+            # in value already in the tree
+            if value == temp.value:
+                return False
+            # pointer goes to the right
+            if value > temp.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            # pointer goes to the left
+            elif value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+
+my_b_tree = BinarySearchTree()
+print(my_b_tree.insert(2))
+print(my_b_tree.insert(1))
+print(my_b_tree.insert(3))
+print(my_b_tree.insert(3))
